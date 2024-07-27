1bd1ec687a69a98bae39f99ca87d2fcc53495850
Ali Hamza <hamzamashooq492@gmail.com>
2024-07-27 12:53:39 +0500

    Functions completed

--- class03.py
+++ class03.py
@@ -9,6 +9,17 @@ def add(a,b):
     result = num1 + num2
     print(result)
 
+def sum(a,b):
+    c = a + b
+    return c
+    
+
 show();
 add(20,45);
+add(sum(10,20),30)
 
+result = sum(10,20) + 10
+print(result)
+
+#  build-in function local
+print(locals())
---
+++ class04.py
@@ -0,0 +1,8 @@
+# in operator
+userName = "Ali Hamza"
+value = 'H'
+result = value in userName
+print(result)
+
+result = value not in userName
+print(result)
